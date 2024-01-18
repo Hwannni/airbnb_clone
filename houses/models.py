2358f5b62ab2581fff261d0d2e80553013270fb7
Hwannni <cmhcms1115@naver.com>
2024-01-18 20:32:05 +0900

    2024.01.18

--- houses/models.py
+++ houses/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 class House(models.Model):
     """Model Definition for Houses"""
-    
+
     name = models.CharField(max_length=140)
     price = models.PositiveBigIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
