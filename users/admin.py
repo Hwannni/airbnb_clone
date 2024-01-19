7012935d7b774ff0a1837763f39d8a75d3e6b244
Hwannni <cmhcms1115@naver.com>
2024-01-19 23:58:09 +0900

    2024.01.19_user.admin페이지셋팅

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+
+@admin.register(User) # 이 클래스가 user 모델을 관리한다
+class CustomUserAdmin(UserAdmin):
+    pass
