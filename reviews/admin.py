0c93b2d81011bcb1d2de0c30d3d56c2e0c6b039d
Hwannni <cmhcms1115@naver.com>
2024-01-28 23:57:55 +0900

    2024.01.28

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,11 +1,29 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
 from .models import Review
 
 # Register your models here.
 
-class WordFilter(admin.SimpleListFilter):
 
+class WordFilter(admin.SimpleListFilter):
     title = "Filter by words!"
+    parameter_name = "word"
+
+    # lookups는 항상 튜플 리스트를 반환해야 한다.
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            reviews
 
 
 @admin.register(Review)
@@ -15,6 +33,7 @@ class ReviewAdmin(admin.ModelAdmin):
         "payload",
     )
     list_filter = (
+        WordFilter,
         "rating",
         # Foreign Key 관계로 필터 만들기
         "user__is_host",
