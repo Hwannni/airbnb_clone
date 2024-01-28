f86aed4a90cb055d799811a7d66bcc2e17a781bf
Hwannni <cmhcms1115@naver.com>
2024-01-28 23:37:10 +0900

    2024.01.28

--- reviews/admin.py
+++ reviews/admin.py
@@ -3,6 +3,10 @@ from .models import Review
 
 # Register your models here.
 
+class WordFilter(admin.SimpleListFilter):
+
+    title = "Filter by words!"
+
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
@@ -10,4 +14,10 @@ class ReviewAdmin(admin.ModelAdmin):
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        "rating",
+        # Foreign Key 관계로 필터 만들기
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )
--- rooms/admin.py
+++ rooms/admin.py
@@ -4,13 +4,26 @@ from .models import Room, Amenity
 # Register your models here.
 
 
+# action은 3개의 파라미터를 충족시켜야함
+# 1. 액션을 호출하는 클래스
+# 2. request 객체
+# 3. Queryset(내가 선택한 모든 객체의 리스트)
+@admin.action(description="Set all prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
     list_display = (
         "name",
         "price",
         "kind",
         "total_amenities",
+        "rating",
         "owner",
         "created_at",
     )
@@ -23,6 +36,12 @@ class RoomAdmin(admin.ModelAdmin):
         "amenities",
         "created_at",
         "updated_at",
+    )
+
+    search_fields = (
+        "name",
+        "^price",
+        "=owner__username",
     )
 
 
