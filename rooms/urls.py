1911bca059c8cce8994c0fa451475d94068f7185
Hwannni <cmhcms1115@naver.com>
2024-02-01 20:33:03 +0900

    2024.02.01_room_detail.html생성_views.py_수정

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.see_all_rooms),
-    path("<int:room_id>", views.see_one_room),
+    # 변수를 받을 땐 <> 사용(타입, 파라미터)
+    path("<int:room_pk>", views.see_one_room),
 ]
